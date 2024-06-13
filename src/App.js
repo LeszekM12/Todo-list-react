d41013a1951e97b6a8c0b4e9446577bade1d4283
Leszek Mikrut <leszekmikrut60@gmail.com>
2024-06-13 12:40:05 +0200

    toggleTaskDone function added

--- src/App.js
+++ src/App.js
@@ -21,6 +21,16 @@ function App() {
     setTasks(tasks => tasks.filter(tasks => tasks.id !== id));
   };
 
+  const toggleTaskDone = (id) => {
+    setTasks(tasks => tasks.map(task => {
+      if(task.id === id ) {
+        return { ...task, done: !task.done };
+      }
+
+      return task;
+    }));
+  };
+
   return (
     <Container>
       <Header title="Lista zadań" />
@@ -32,7 +42,9 @@ function App() {
         <Tasks 
           tasks={tasks} 
           hideDone={hideDone} 
-          removeTask={removeTask}/>
+          removeTask={removeTask}
+          toggleTaskDone={toggleTaskDone}
+          />
         }
         extraHeaderContent={
           <Buttons
--- src/Tasks/index.js
+++ src/Tasks/index.js
@@ -1,7 +1,6 @@
 import "./style.css";
 
-
-const Tasks = ({ tasks, hideDone, removeTask }) => (
+const Tasks = ({ tasks, hideDone, removeTask, toggleTaskDone }) => (
   <ul className="tasks">
     {tasks.map((task) => (
       <li
@@ -10,19 +9,25 @@ const Tasks = ({ tasks, hideDone, removeTask }) => (
           task.done && hideDone ? " tasks__item--hidden" : ""
         }`}
       >
-        <button className="tasks__button tasks__button--toggleDone">
-                {task.done ? "" : "🗸"}
-            </button>
-            <span className={`tasks__content ${
-              task.done ? "tasks__content--done" : ""}"`}>
-                {task.content}
-            </span>
-            <button 
-            className="tasks__button tasks__button--remove"
-            onClick={() => removeTask(task.id)}
-            >
-              🗑️
-            </button>
+        <button
+          className="tasks__button tasks__button--toggleDone"
+          onClick={() => toggleTaskDone(task.id)}
+        >
+          {task.done ? "" : "🗸"}
+        </button>
+        <span
+          className={`tasks__content ${
+          task.done ? "tasks__content--done" : ""}`
+          }
+        >
+          {task.content}
+        </span>
+        <button
+          className="tasks__button tasks__button--remove"
+          onClick={() => removeTask(task.id)}
+        >
+          🗑️
+        </button>
       </li>
     ))}
     <li className="tasks__item"></li>
@@ -30,4 +35,3 @@ const Tasks = ({ tasks, hideDone, removeTask }) => (
 );
 
 export default Tasks;
-
