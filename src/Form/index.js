23d7a461d3713b35770f6f863662e6a84fa2b068
Leszek Mikrut <leszekmikrut60@gmail.com>
2024-06-13 14:55:45 +0200

    empty task blocked

--- src/Form/index.js
+++ src/Form/index.js
@@ -6,6 +6,9 @@ const Form = ({addNewTask}) => {
 
   const onFormSubmit = (event) => {
     event.preventDefault();
+    if (!newTaskContent.trim()) {
+      return;
+    }
     addNewTask(newTaskContent.trim());
     setNewTaskContent("");
   };
