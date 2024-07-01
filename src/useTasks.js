238159923c182fa4942cd86e3363ac7b952ac75b
Leszek Mikrut <leszekmikrut60@gmail.com>
2024-07-01 14:53:42 +0200

    useTask added

---
+++ src/useTasks.js
@@ -0,0 +1,45 @@
+import React, { useEffect, useState } from "react";
+import { defaultTasks } from "./Tasks";
+
+export const useTasks = () => {
+    const localStorageTasks = localStorage.getItem("tasks");
+
+  const [tasks, setTasks] = useState(
+    localStorageTasks ? JSON.parse(localStorageTasks) : defaultTasks
+  );
+
+  useEffect(() => {
+    localStorage.setItem("tasks", JSON.stringify(tasks));
+  }, [tasks]);
+  
+  const removeTask = (id) => {
+    setTasks(tasks => tasks.filter(tasks => tasks.id !== id));
+  };
+
+  const toggleTaskDone = (id) => {
+    setTasks(tasks => tasks.map(task => {
+      if(task.id === id) {
+        return { ...task, done: !task.done };
+      }
+
+      return task;
+    }));
+  };
+
+  const markAllTaskDone = () => {
+    setTasks(tasks => tasks.map(task => (
+        {...task, done: true }
+    )));
+  };
+
+  const addNewTask = (newTaskContent) => {
+    setTasks(tasks => [
+      ...tasks, 
+      { 
+        content: newTaskContent, 
+        done: false, 
+        id: tasks.length ? tasks[tasks.length - 1].id + 1 : 1,
+      },
+    ]);
+  };
+};
