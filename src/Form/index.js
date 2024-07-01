7c656ff9791a43592ea8573a4fda7732967ca111
Leszek Mikrut <leszekmikrut60@gmail.com>
2024-07-01 13:31:12 +0200

    useRef interaction

--- src/Form/index.js
+++ src/Form/index.js
@@ -1,8 +1,9 @@
-import { useState } from "react";
+import { useState, useRef } from "react";
 import "./style.css";
 
 const Form = ({addNewTask}) => {
   const [newTaskContent, setNewTaskContent] = useState("");
+  const inputRef = useRef(null);
 
   const onFormSubmit = (event) => {
     event.preventDefault();
@@ -16,12 +17,16 @@ const Form = ({addNewTask}) => {
   return (
     <form className="form" onSubmit={onFormSubmit}>
       <input
+        ref={inputRef}
         value={newTaskContent}
         className="form__input"
         placeholder="Co jest do zrobienia?"
         onChange={({ target }) => setNewTaskContent(target.value)}
       />
-      <button className="form__button">Dodaj zadanie</button>
+      <button 
+        onClick={() => inputRef.current.focus()}
+        className="form__button">Dodaj zadanie
+      </button>
     </form>
   );
 };
