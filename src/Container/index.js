2c954d5ea7abc2a8b42e124022efb3c78a14181c
Leszek Mikrut <leszekmikrut60@gmail.com>
2024-07-03 11:34:01 +0200

    Styled Container

--- src/Container/index.js
+++ src/Container/index.js
@@ -1,9 +1,9 @@
-import "./style.css";
+import { StyledContainer } from "./styled";
 
 const Container = ({ children} ) => (
-    <main className="container">
+    <StyledContainer>
         {children}
-    </main>
+    </StyledContainer>
 );
 
 export default Container;
--- src/Container/styled.js
+++ src/Container/styled.js
@@ -1,6 +1,8 @@
-.container {
+import styled from "styled-components";
+
+export const StyledContainer = styled.main`
     max-width: 900px;
     padding: 10px;
     margin: 0 auto;
     overflow-wrap: anywhere;
-}
+`;
