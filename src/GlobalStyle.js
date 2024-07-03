869bb39fa2ba42e580802190f59b9197de51a3c2
Leszek Mikrut <leszekmikrut60@gmail.com>
2024-07-03 13:48:50 +0200

    Global Style

---
+++ src/GlobalStyle.js
@@ -0,0 +1,21 @@
+import { createGlobalStyle } from "styled-components";
+
+export const GlobalStyle = createGlobalStyle`
+    html {
+        box-sizing: border-box;
+    }
+
+    *, ::after, ::before {
+        box-sizing: inherit;
+    }
+
+    button {
+        cursor: pointer;
+    }  
+
+    body {
+        font-family: 'Lato', sans-serif;
+        max-width: 1000px;
+        margin: 0 auto;
+    }
+`;
