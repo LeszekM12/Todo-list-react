99160ca7922f9c91780d94c86f24a3ad49fb72c1
Leszek Mikrut <leszekmikrut60@gmail.com>
2024-07-17 13:58:41 +0200

    Provider react redux

--- src/index.js
+++ src/index.js
@@ -5,14 +5,18 @@ import { theme } from './theme';
 import { ThemeProvider } from 'styled-components';
 import { GlobalStyle } from "./GlobalStyle";
 import { App } from './App';
+import { Provider } from 'react-redux';
+import store from './store';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
+    <Provider store={store}>
     <ThemeProvider theme={theme}>
       <GlobalStyle />
       <App />
     </ThemeProvider>
+    </Provider>
   </React.StrictMode>
 );
 
