b3d4e1e386e5ddc891b535aeb6912ea9faf4340d
Leszek Mikrut <leszekmikrut60@gmail.com>
2024-07-19 13:37:40 +0200

    store

--- src/store.js
+++ src/store.js
@@ -1,8 +1,18 @@
 import { configureStore } from "@reduxjs/toolkit";
+import createSagaMiddleware from "redux-saga";
 import tasksReducer from './features/tasks/tasksSlice';
+import { watchAxiosExampleTasks } from "./features/tasks/tasksSaga";
 
-export default configureStore({
+const sagaMiddleware = createSagaMiddleware();
+
+const store = configureStore({
     reducer: {
         tasks: tasksReducer,
     },
+    middleware: (getDefaultMiddleware) =>
+        getDefaultMiddleware().concat(sagaMiddleware),
 });
+
+sagaMiddleware.run(watchAxiosExampleTasks);
+
+export default store;
