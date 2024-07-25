a7466d3be5f8bcf920557ea92d7dfe6b8e365959
Leszek Mikrut <leszekmikrut60@gmail.com>
2024-07-25 11:20:14 +0200

    response data

--- src/features/tasks/getExampleTasks.js
+++ src/features/tasks/getExampleTasks.js
@@ -3,5 +3,5 @@ import axios from "axios";
 export const getExampleTasks = async () => {
     const response = await axios.get("/Todo-list-react/exampleTasks.json");
 
-    return response;
+    return response?.data;
 };
