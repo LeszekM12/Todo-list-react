8adf46c9955e18f1d0f8998eebc39bdab7cdf752
Leszek Mikrut <leszekmikrut60@gmail.com>
2024-08-04 20:40:49 +0200

    styled nav

--- src/common/NavigationBar/styled.js
+++ src/common/NavigationBar/styled.js
@@ -3,8 +3,7 @@ export const List = styled.ul``;
 export const StyledNavLink = styled(NavLink).attrs(() => ({
     activeClassName,
 }))`
-    margin: 5px;
-    flex-basis: 30%; 
+    
     text-align: center;
 
 `;
