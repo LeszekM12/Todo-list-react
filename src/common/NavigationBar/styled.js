46b253ec9b30e57c1d63e0b7d0f4f35df1fd81c8
Leszek Mikrut <leszekmikrut60@gmail.com>
2024-08-07 14:48:10 +0200

    nav styled

--- src/common/NavigationBar/styled.js
+++ src/common/NavigationBar/styled.js
@@ -1,15 +1,22 @@
 import styled from "styled-components";
 import { NavLink } from "react-router-dom/cjs/react-router-dom.min";
 
-export const Navigation = styled.nav`
-    border: 1px solid black;
-`;
-
-export const List = styled.ul`
+export const Navigation = styled.ul`
+    border: 1px solid teal;
     background-color: teal;
+    margin: 0;
+    padding: 0;
+    display: flex;
+    justify-content: center;
+    list-style: none;
 `;
 
-const activeClassName = "active";
+export const List = styled.li`
+    background-color: teal;
+    margin: 20px;
+`;
+
+const activeClassName = "active-link";
 
 export const StyledNavLink = styled(NavLink).attrs(() => ({
   activeClassName,
@@ -18,13 +25,18 @@ export const StyledNavLink = styled(NavLink).attrs(() => ({
   color: black;
   text-decoration: none;
   transition: 0.1s;
-  padding:  0px 10px;
-  line-height: 1.5;
+  padding: 10px;
+  line-height: 2;
+  font-size: 17px;
 
   &.${activeClassName} {
       background-color: white;
-      border: 2px solid rgba(179, 174, 174, 0.742);
-      border-radius: 5px;
-      font-weight: 600;
+      border: 5px solid white;
+      border-radius: 30px;
+      font-weight: bold;
+  }
+
+  &:hover {
+    border-bottom: 1px solid white;
   }
 `;
