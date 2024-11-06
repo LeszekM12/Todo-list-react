a40c7d3ac093a03cbfd59ef05c382727533a502a
Leszek Mikrut <leszekmikrut60@gmail.com>
2024-11-06 15:13:28 +0100

    tsconfig moved to global todo-list-react

--- src/index.tsx
+++ src/index.tsx
@@ -8,7 +8,7 @@ import { App } from './core/App';
 import { Provider } from 'react-redux';
 import store from './core/store';
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+const root = ReactDOM.createRoot(document.getElementById('root')!);
 root.render(
   <React.StrictMode>
     <Provider store={store}>
