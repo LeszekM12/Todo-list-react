d96917e83645a4493a1d377a39acf0507e10c98f
Leszek Mikrut <leszekmikrut60@gmail.com>
2024-11-13 14:34:29 +0100

    Changed theme to ts file and switched as a const.

--- src/core/theme.js
+++
@@ -1,8 +0,0 @@
-export const theme = {
-    color: {
-        teal: "rgb(0, 128, 128)"
-    },
-    breakpoints: {
-        mobileMax: 767,
-    },
-};
---
+++ src/core/theme.ts
@@ -0,0 +1,15 @@
+
+export const theme = {
+    color: {
+        black: '#000000',
+        crimson: '#db143c',
+        teal: '#008080',
+        alto: '#ddd',
+        gallery: '#eee',
+        silver: '#ccc',
+        white: '#fff',
+    },
+    breakpoints: {
+        mobileMax: "767px",
+    },
+} as const;
