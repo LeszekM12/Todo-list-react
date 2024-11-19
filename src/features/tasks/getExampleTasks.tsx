4c4609550b2a8f3dd917a2919c03949d1c449b96
Leszek Mikrut <leszekmikrut60@gmail.com>
2024-11-19 15:02:43 +0100

    types interface Task imported to getExampleTasks response data

--- src/features/tasks/getExampleTasks.tsx
+++ src/features/tasks/getExampleTasks.tsx
@@ -1,7 +1,8 @@
 import axios from "axios";
+import {Task} from "../../types";
 
 export const getExampleTasks = async () => {
     const response = await axios.get("/Todo-list-react/exampleTasks.json");
 
-    return response?.data;
+    return (response?.data) as Task[];
 };
