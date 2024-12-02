8d2b25d0abf722aab1172865a597e230344d54b8
Leszek Mikrut <leszekmikrut60@gmail.com>
2024-12-02 14:54:39 +0100

    queryParameters: changed to typescript

--- src/queryParameters.ts
+++ src/queryParameters.ts
@@ -1,7 +1,7 @@
-import { useLocation } from "react-router-dom/cjs/react-router-dom.min";
-import { useHistory } from "react-router-dom/cjs/react-router-dom";
+import { useLocation } from "react-router-dom";
+import { useHistory } from "react-router-dom";
 
-export const useQueryParameter = key => {
+export const useQueryParameter = (key: string)  => {
     const location = useLocation();
     const searchParams = new URLSearchParams(location.search);
     return searchParams.get(key);
@@ -12,7 +12,7 @@ export const useReplaceQueryParameter = () => {
     const location = useLocation();
     const history = useHistory();
 
-    return({ key, value }) => {
+    return({ key, value }: {key: string; value: string | undefined}) => {
         const searchParams = new URLSearchParams(location.search);
         
         if(value === undefined){
